f9d5361ea319f19f0fb6f6a89766d7694e81b26e
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2025-06-04 14:43:15 +0530

    the issue of showing message status(read receipts on both side) is resolved, only show the read receipts for message on the sender side

--- src/components/chat_Area/MsgBox.jsx
+++ src/components/chat_Area/MsgBox.jsx
@@ -1,12 +1,14 @@
 import { MessageBox } from "react-chat-elements";
 
 const MsgBox = ({ message, isSender }) => {
+// only show the message status(read receipts) for the sender's messages(only on sender's side)
+const showStatus = isSender && message?.status;
   return (
     <MessageBox
       type={"text"}
       text={message.msg}
       position={isSender ? "right" : "left"}
-      status={message?.status}
+      status={showStatus ? message?.status : undefined}
     />
   );
 };
